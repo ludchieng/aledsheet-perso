aa6b7168d0d6a9cbb7049fc6a6d0af6002abbeab
ludchieng <lud.chieng@hotmail.fr>
2021-08-04 20:43:47 +0200

    [all] Add index tags

--- .eleventy.js
+++ .eleventy.js
@@ -7,14 +7,21 @@ module.exports = function (config) {
   }));
 
   config.addCollection('pageTags', (collections) => {
-		const uniqueTags = collections
+		const uniqueTags = [...new Set(collections
 			.getAll()
 			.reduce((tags, item) => tags.concat(item.data.tags), [])
 			.filter((tag) => !!tag)
 			.filter((tag) => !!tag && !['page', 'post'].includes(tag))
-			.sort();
-		return Array.from(new Set(uniqueTags));
+			.sort())];
+		return uniqueTags;
 	});
+  config.addFilter('capitalize', (str) => {
+		return str.trim().toLowerCase()
+      .replace(/\w\S*/g, (w) => (
+        w.replace(/^\w/, (c) => c.toUpperCase())
+      ));
+	});
+
 
   config.addPassthroughCopy("styles");
 
